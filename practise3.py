76c0c2a760311753c6dce397798fdf42bd2d1f6f
bob <az8715555@gmail.com>
2025-11-27 13:21:12 +0800

    第一次備份

---
+++ practise3.py
@@ -0,0 +1,10 @@
+name = ["Alice", "Bob", "Charlie", "David", "Eva"]
+print(name)  #顯示字串
+print(name.lower())
+#將字串轉換為小寫
+print(name.title())
+#將字串的每個單字首字母大寫
+print(name.upper())
+#將字串轉換為大寫
+print(name.swapcase())
+#將字串的大寫轉換為小寫，小寫轉換為大寫
